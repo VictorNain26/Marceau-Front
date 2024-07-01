2a0cf73b7ea46146a3737cf59b4b455a1604c6af
victor lenain <victor.lenain26@gmail.com>
2024-07-01 17:05:40 +0200

    [master] change hero banner design

--- src/app/page.js
+++ src/app/page.js
@@ -1,17 +1,60 @@
-import Header from '../components/Header';
+"use client"
+
+import { motion, useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
+
+import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 export default function Home() {
+  const videoRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: videoRef,
+    offset: ["end", "start"]
+  });
+
+  useEffect(() => {
+    scrollYProgress.on("change", e => console.log(e));
+  }, []);
+
   return (
     <>
     <main>
-      <Header />
+      <section className="flex">
+        <header
+          className='p-7 w-1/5 flex flex-col text-center h-screen text-white bg-black'
+        >
+          <span className='mb-auto pt-20 text-4xl'>
+            MARCEAU LUTIN
+          </span>
 
-      <div className="grid justify-center min-h-screen bg-red-600">
-        <h1>Les projets</h1>
-      </div>
-      <div className="grid justify-center min-h-screen bg-green-600">
-        <h1>Les projets</h1>
-      </div>
+          <ul className='text-xl my-auto'>
+            <li>Réalisateur</li>
+            <li className="my-5">Monteur</li>
+            <li className="mb-5">Cadreur</li>
+            <li>Photographe</li>
+          </ul>
+
+          <div className="text-xs mt-auto">
+            <span>@MARCEAULUTIN</span>
+          </div>
+        </header>
+
+        <video
+          src="./videos/showreel.mp4"
+          loop autoPlay muted
+          className={`object-cover right-0 fixed h-screen w-4/5 -z-10`}
+        >
+          <motion.div style={{opacity: scrollYProgress}}>
+            <ChevronDownIcon className="fixed bottom-0 right-0 left-0 -z-10 w-12 animate-bounce text-white mx-auto" />
+          </motion.div>
+        </video>
+      </section>
+
+      {/* <section className="grid justify-center min-h-screen">
+        <div>
+
+        </div>
+      </section> */}
     </main>
     </>
   );
--- src/components/Header.js
+++ src/components/Header.js
@@ -2,31 +2,37 @@
 "use client"
 
 import { motion, useScroll } from "framer-motion";
+import { useEffect, useRef } from "react";
 
 import { ChevronDownIcon } from '@heroicons/react/24/outline';
 
 const Header = () => {
-  const { scrollYProgress } = useScroll();
+  const videoRef = useRef(null)
+  const { scrollYProgress } = useScroll({
+    target: videoRef,
+    offset: ["end", "start"]
+  });
+
+  useEffect(() => {
+    scrollYProgress.on("change", e => console.log(e));
+  }, []);
 
   return (
     <>
-      <motion.div
-        className="fixed top-0 left-0 right-0 h-2 bg-violet-700"
-        style={{ scaleX: scrollYProgress }}
-      />
-
-      <video src="https://res.cloudinary.com/dblbgz4ym/video/upload/v1719612508/SHOWREEL_MARCEAU_LUTIN_2024_c885e307ad.mp4" loop autoPlay muted className='object-cover fixed h-screen w-screen -z-10'></video>
-
-      <header className='px-72 pt-72 h-screen flex flex-col text-white'>
+      <header
+        className='px-72 pt-72 h-screen flex flex-col text-white'
+      >
         <span className='sticky top-0 pt-5 text-6xl'>
           MARCEAU LUTIN
         </span>
 
-        <span className='text-neutral-200'>
+        <motion.span className='text-neutral-200' style={{opacity: scrollYProgress}}>
           Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-        </span>
+        </motion.span>
 
-        <ChevronDownIcon className="w-12 animate-bounce text-white mx-auto mt-auto" />
+        <motion.div style={{opacity: scrollYProgress}}>
+          <ChevronDownIcon className="fixed bottom-0 right-0 left-0 -z-10 w-12 animate-bounce text-white mx-auto" />
+        </motion.div>
       </header>
     </>
   );
