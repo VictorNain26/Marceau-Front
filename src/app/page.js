9285a0baa5b39c6064b6759c33d51b0277b74e75
victor lenain <victor.lenain26@gmail.com>
2024-07-03 19:50:32 +0200

    [master] add images masonry

--- src/app/page.js
+++ src/app/page.js
@@ -1,22 +1,16 @@
 "use client"
 
 import { ChevronDownIcon, ChevronLeftIcon } from "@heroicons/react/24/outline";
-import { motion, useScroll } from "framer-motion";
-import { useEffect, useRef, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 
 import Image from "next/image"
 import Link from "next/link";
+import { useInView } from "react-intersection-observer";
 
 export default function Home() {
   const [isActive, setActive] = useState(true);
-  const videoRef = useRef(null)
-  const { scrollYProgress } = useScroll({
-    target: videoRef,
-    offset: ["end", "start"]
-  });
-
-  useEffect(() => {
-    console.log(isActive);
+  const { ref, inView } = useInView({
+    threshold: 0.05,
   });
 
   const toggleClass = () => {
@@ -28,7 +22,7 @@ export default function Home() {
     <main>
       <header
         className={
-          `fixed top-0 flex flex-col text-center h-screen ease-linear duration-200 text-zinc-50 bg-stone-950 -z-10
+          `fixed top-0 flex flex-col text-center h-screen ease-in-out duration-300 text-zinc-50 bg-stone-950
           ${isActive ? "w-1/5 p-7" : "w-0 p-0"}`
         }
       >
@@ -49,7 +43,11 @@ export default function Home() {
               <Image src="/icons/instagram.svg" width={30} height={30} alt="Instagram icon" />
             </Link>
 
-            <Link href="https://www.youtube.com/channel/UCbseNjiHKQNT_mwCNj3Y6Rg" target="_blank" className="ml-1 cursor-pointer">
+            <Link
+              href="https://www.youtube.com/channel/UCbseNjiHKQNT_mwCNj3Y6Rg"
+              target="_blank"
+              className="ml-1 cursor-pointer"
+            >
               <Image src="/icons/youtube.svg" width={30} height={30} alt="Youtube icon" />
             </Link>
           </div>
@@ -58,17 +56,21 @@ export default function Home() {
         </div>
       </header>
 
-      <div className={ `h-screen ease-linear duration-200 ml-auto ${isActive ? "w-4/5" : "w-full"}` }>
-        <video
-          src="./videos/showreel.mp4"
-          autoPlay loop muted
-          className={`object-cover fixed right-0 ease-linear duration-200 h-screen -z-10 ${isActive ? "w-4/5" : "w-full"}`}
-        >
-        </video>
+      <video
+        src="./videos/showreel.mp4"
+        autoPlay loop muted
+        className={
+          `object-cover fixed right-0 ease-in-out duration-300 h-screen
+          ${isActive ? "w-4/5" : "w-full"}
+          ${inView ? "brightness-[0.2]" : ""}`
+        }
+      >
+      </video>
 
+      <div className={ `h-screen ease-in-out duration-300 ml-auto ${isActive ? "w-4/5" : "w-full"}` }>
         <ChevronLeftIcon
           className={
-            `fixed w-12 text-zinc-50 cursor-pointer ease-linear duration-300 h-full z-10
+            `fixed w-12 text-zinc-50 cursor-pointer ease-in-out duration-300 h-full z-10
             ${isActive ? "" : "rotate-180"}`
           }
           onClick={toggleClass}
@@ -76,15 +78,108 @@ export default function Home() {
 
         <ChevronDownIcon
           className={
-            `fixed bottom-0 right-0 h-12 animate-bounce ease-linear duration-200 text-zinc-50
-            ${isActive ? "w-4/5" : "w-full"}`
+            `fixed bottom-0 right-0 h-12 animate-bounce ease-in-out duration-300 text-zinc-50
+            ${isActive ? "w-4/5" : "w-full"}
+            ${inView ? "opacity-0" : ""}`
           }
         />
       </div>
 
-      <section className={ `min-h-screen bg-black opacity-30 ease-linear duration-200 ml-auto ${isActive ? "w-4/5" : "w-full"}` }>
-        <div>
+      <section
+        ref={ref}
+        className={
+          `masonry ease-in-out duration-300 py-10
+          ${isActive ? "w-4/5 ml-auto" : "w-5/6 mx-auto"}`
+        }
+      >
+        <div className="relative">
+          <Image
+            src="/images/DSC03379.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
 
+        <div className="relative">
+          <Image
+            src="/images/DSC03385.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/DSC03379.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/DSC03385.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/DSC03379.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/DSC03379.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/DSC03385.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/DSC03385.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
+        </div>
+
+        <div className="relative">
+          <Image
+            src="/images/DSC03379.jpg"
+            alt="Picture of the author"
+            width={400}
+            height={0}
+            className="rounded-lg break-inside mb-4 mx-auto"
+          />
         </div>
       </section>
     </main>
